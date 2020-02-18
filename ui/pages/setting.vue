0eb9388c0fc24591f308850c4c29b571746e83ff
rkamio <rkamio@rkamionoMacBook-Pro.local>
2020-02-18 12:32:37 +0900

    Mock-Up controll page

---
+++ ui/pages/setting.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1>設定</h1>
+        <br />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<!-- <script>
+export default {
+  
+}
+</script> -->
